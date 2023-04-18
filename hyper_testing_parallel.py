01af4a0d48368ec17918d7e9dbbb78015f405ea0
Jordy <jordyschifferstein@hotmail.com>
2023-04-18 09:54:49 +0200

    Update from aesopus

--- hyper_testing_parallel.py
+++ hyper_testing_parallel.py
@@ -6,11 +6,7 @@ import time
 box_center = (1., 21.8, 36.3) # Active site coordinates 
 box_size = (30,30,30)
 
-to_dock = pd.read_csv('input/230317_KLIFS_Ligands.csv')
-
-# test_pdb = to_dock['Structure ID'].unique()[0]
-# test_smiles_df = to_dock[to_dock['Structure ID'] == test_pdb]
-# test_smiles = test_smiles_df['SMILES'].tolist()
+to_dock = pd.read_csv('input/230406_KLIFS_Ligands_VINA.csv')
 
 # THREAD NUMBER MUST BE DIVISIBLE BY 32!
 
@@ -19,21 +15,22 @@ search_depths = list(range(5, 31, 5))
 
 for num_threads in threads:
     for depth in search_depths:
-        '''SKIPPING 256 + 5 and 256 + 10 because they were already done on 1 GPU'''
-        if num_threads == 256 and (depth == 5 or depth == 10):
-            continue
-
         print('-'*50)
         print(f'Threads: {num_threads}\nSearch_depth: {depth}')
         print('-'*50)
         sub_folder = f'{num_threads}_{depth}'
 
-        t0 = time.time()
-
         for pdb in to_dock['Structure ID'].unique():
             print("Currently working on target:", pdb)
+
+            t0 = time.time()
+
             target_pdb_path = os.path.join('input', 'pdbs', str(pdb)+'.pdb')
             output_subfolder = '_'.join([str(pdb), sub_folder])
+
+            # SKIP EXISTING RUNS
+            if os.path.exists(f'output/{output_subfolder}/log.tsv'):
+                continue
 
             smiles_df = to_dock[to_dock['Structure ID'] == pdb] 
             smiles = smiles_df['SMILES'].tolist()
@@ -49,12 +46,17 @@ for num_threads in threads:
                 threads=num_threads, 
                 threads_per_call=num_threads,
                 verbose=False,
-                gpu_ids=[0,1],
+                gpu_ids=[0],
                 workers_per_gpu=2,
                 num_cpu_workers=0)
 
+            t_spend = time.time() - t0  # Measured time per klifs structure
 
-        t_spend = time.time() - t0
+            try:
+                with open(f'output/{num_threads}_{depth}_timing.txt', 'r') as f:
+                    prev_time = float(f.read().strip())
+            except (FileNotFoundError, ValueError):
+                prev_time = 0
 
-        with open(f'output/{num_threads}_{depth}_timing.txt', 'w') as f:
-            f.write(str(t_spend))
+            with open(f'output/{num_threads}_{depth}_timing.txt', 'w') as f:
+                f.write(str(t_spend + prev_time))
